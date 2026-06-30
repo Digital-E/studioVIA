f192d600ddd46d94d29b43142de4d73bf8fd2e97
SamuelBassett <samabassett@gmail.com>
2026-06-30 13:31:40 +0200

    update

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'r0r7pkb1'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production'
 
 export const client = createClient({
   projectId,
